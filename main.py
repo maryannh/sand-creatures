e7fffe831e538fbbe05f1669d4c792174574b80d
genlog <shall.be.lifted+genlog@gmail.com>
2016-07-17 20:31:39 +0200

    initial
    :thumb:20160717-203118-640472-b8c9e92-55c5878.png

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ ONE = 1./SIZE
 BACK = [1,1,1,1]
 FRONT = [0,0,0,0.05]
 
-INUM = 5
+PNUM = 5
+INUM = 1000
+
 
 def make_creatures(sand):
-  from creature import Creature
+  from modules.creature import Creature
 
-  creature = Creature(SIZE, INUM)
-
-  # dots = creature.step()
+  creature = Creature(SIZE, PNUM, INUM)
+  l1, l2 = creature.paths()
+  sand.paint_strokes(l1, l2, INUM)
 
 
 def main():
@@ -28,23 +30,9 @@ def main():
   fn = Fn(prefix='./res/', postfix='.png')
 
   make_creatures(sand)
-
-  # try:
-  #   while True:
-  #     t0 = time()
-  #     itt = dunes.steps(LEAP)
-  #     print(itt, time()-t0)
-  #     dunes.get_normalized_sand_limit(bw, 10)
-  #     # bw *= 0.8
-  #     # sand.set_bg_from_bw_array(bw)
-  #     # dunes.get_shadow(shadow)
-  #     # rgb = dstack((bw,bw,shadow))
-  #     # sand.set_bg_from_rgb_array(rgb)
-  #     sand.set_bg_from_bw_array(bw)
-  #     name = fn.name()
-  #     sand.write_to_png(name)
-  # except KeyboardInterrupt:
-  #   pass
+  # sand.set_bg_from_bw_array(bw)
+  name = fn.name()
+  sand.write_to_png(name)
 
 
 if __name__ == '__main__':
--- modules/creature.py
+++ modules/creature.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from numpy import pi
-from numpy import column_stack
-from numpy import sin
-from numpy import cos
-from numpy import reshape
 from numpy import zeros
 from numpy.random import random
 
@@ -13,53 +8,32 @@ from .helpers import _rnd_interpolate
 ORDERED = True
 
 
-TWOPI = pi*2
-HPI = pi*0.5
-
 class Creature(object):
   def __init__(
       self,
+      size,
+      pnum,
       inum
       ):
     self.i = 0
+    self.size = size
+    self.pnum = pnum
     self.inum = inum
 
-    # self.interpolated_path = _rnd_interpolate(path, inum, ordered=ORDERED)
+    rnd = 0.1 + random((2*pnum-1, 2))*0.8
 
-    rnd = random((2*inum-1, 2))
-
-    self.xy1 = rnd[:inum,:]
-    self.xy2 = zeros((inum,2), 'float')
+    self.xy1 = rnd[:pnum,:]
+    self.xy2 = zeros((pnum,2), 'float')
     self.xy2[0,:] = rnd[0,:]
-    self.xy2[-1,:] = rnd[inum-1,:]
-    self.xy2[1:-1,:] = rnd[inum:-1,:]
+    self.xy2[-1,:] = rnd[pnum-1,:]
+    self.xy2[1:-1,:] = rnd[pnum:-1,:]
 
-    print(self.xy1)
-    print(self.xy2)
 
   def __iter__(self):
       return self
 
-  # def __next__(self):
-  #
-  #   if self.i<
-  #
-  #
-  #     g = next(self.guide)
-  #   except Exception:
-  #     raise StopIteration
-  #
-  #   pnum = self.pnum
-  #
-  #   r = 1.0-2.0*random(pnum)
-  #   self.noise[:] += r*self.scale
-  #
-  #   a = random(pnum)*TWOPI
-  #   rnd = column_stack((cos(a), sin(a)))
-  #
-  #   self.path += rnd * reshape(self.noise, (self.pnum,1))
-  #   self.interpolated_path = _rnd_interpolate(self.path, self.inum, ordered=ORDERED)
-  #
-  #   self.i+=1
-  #   return g + self.interpolated_path
-  #
+  def paths(self):
+    l1 = _rnd_interpolate(self.xy1, self.inum, ordered=True)
+    l2 = _rnd_interpolate(self.xy2, self.inum, ordered=True)
+    return l1, l2
+
