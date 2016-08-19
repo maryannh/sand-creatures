212ca8d67dc75bacdca2066c7c2cc8a03f9b87a7
Anders Hoff <inconvergent@gmail.com>
2016-08-19 22:11:29 +0200

    testing.

--- main-multi.py
+++ main-multi.py
@@ -13,12 +13,12 @@ ONE = 1./SIZE
 
 EDGE = 0.1
 
-INUM = 800
-STEPS = 10000
+INUM = 10000
+STEPS = 50000
 
 CREATURE_NUM = 10
 
-NOISE = 0.03
+NOISE = 0.25
 
 GAMMA = 1.6
 
@@ -26,37 +26,36 @@ GAMMA = 1.6
 def make_creatures(sand):
   from modules.creature import MultiCreature
   from numpy import array
-  from modules.helpers import get_colors
+  # from modules.helpers import get_colors
 
-  colors = get_colors('../colors/shimmering.gif')
-  nc = len(colors)
-
-  w = 0
+  # colors = get_colors('../colors/shimmering.gif')
+  # nc = len(colors)
+  # w = 0
 
   pnum = 4
-  for i, y in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
-    for j, x in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
-      print(i,j)
+  # for i, y in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
+  #   for j, x in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
+  # print(i,j)
 
-      pnum = 4+1
-      xy = array((x, y), 'float')
-      size = (1.0-2*EDGE)/CREATURE_NUM*0.1
-      creature = MultiCreature(
-          pnum,
-          INUM,
-          STEPS,
-          xy,
-          size,
-          get_displaced_multi(NOISE, 4+j)
-          )
+  pnum = 4+1
+  xy = array((0.5, 0.5), 'float')
+  # size = (1.0-2*EDGE)/CREATURE_NUM*0.1
+  size = 0.25
+  creature = MultiCreature(
+      pnum,
+      INUM,
+      STEPS,
+      xy,
+      size,
+      get_displaced_multi(NOISE, 100)
+      )
 
-      w += 1
-      rgba = colors[w%nc] + [0.0005]
-      sand.set_rgba(rgba)
+  # w += 1
+  # rgba = colors[w%nc] + [0.0005]
+  # sand.set_rgba(rgba)
 
-      for l1 in creature.paths():
-
-        sand.paint_dots(l1)
+  for l1 in creature.paths():
+    sand.paint_dots(l1)
 
 
 def main():
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from numpy import linspace
 from modules.paths import get_displaced_single
 # from modules.paths import get_rnd_circ
+# from modules.paths import get_connected
 
 BACK = [1,1,1,1]
 FRONT = [0,0,0,0.0001]
@@ -15,14 +16,14 @@ ONE = 1./SIZE
 EDGE = 0.1
 
 INUM = 20000
-GRAINS = 150
+GRAINS = 500
 
-CREATURE_NUM = 15
+CREATURE_NUM = 13
 ORDERED = True
 
 NOISE = 0.005
 
-GAMMA = 1.5
+GAMMA = 1.6
 
 
 def make_creatures(sand):
@@ -30,33 +31,38 @@ def make_creatures(sand):
   from numpy import array
   from numpy.random import randint
   from modules.helpers import get_colors
+  from numpy.random import random
+  from numpy import sqrt
 
   # colors = get_colors('../colors/shimmering.gif')
   # colors = get_colors('../colors/tumblr_nkpio2Dl4H1rlz4gso2_1280_1000.jpg')
-  colors = get_colors('../colors/ray-1.jpeg')
+  # colors = get_colors('../colors/ir.jpg')
+  colors = get_colors('../colors/black_green.gif')
   nc = len(colors)
 
   w = 0
-  pnum = 4
-  for i, y in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
-    for j, x in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
+  # for i, y in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
+  #   for j, x in enumerate(linspace(EDGE, 1.0-EDGE, CREATURE_NUM)):
 
-      # pnum = randint(4,10)
-      pnum = 4+j
-      # pnum = 5
-      noise = 0.0030 + j*0.001
+  for i in range(400):
+    print(i)
 
-      xy = array((x, y), 'float')
-      size = 0.024
-      creature = Creature(pnum, INUM, xy, size, get_displaced_single(noise), ORDERED)
-      # creature = Creature(pnum, INUM, xy, size, get_rnd_circ(), ORDERED)
-      l1, l2 = creature.paths()
+    pnum = randint(4,8)
+    # pnum = 4+j
+    # pnum = 5
+    # noise = 0.0030 + j*0.001
 
-      w += 1
-      rgba = colors[w%nc] + [0.0005]
-      sand.set_rgba(rgba)
+    xy = array((EDGE + random()*(1.0-2.0*EDGE),EDGE + random()*(1.0-2.0*EDGE)), 'float')
+    size = 0.03
+    creature = Creature(pnum, INUM, xy, size, get_displaced_single(NOISE), ORDERED)
+    # creature = Creature(pnum, INUM, xy, size, get_rnd_circ(NOISE), ORDERED)
+    # creature = Creature(pnum, INUM, xy, size, get_connected(), ORDERED)
+    l1, l2 = creature.paths()
 
-      sand.paint_strokes(l1, l2, GRAINS)
+    w += 1
+    rgba = colors[w%nc] + [0.0005]
+    sand.set_rgba(rgba)
+    sand.paint_strokes(l1, l2, GRAINS)
 
       # pnum += 1
 
@@ -67,11 +73,12 @@ def main():
 
 
   sand = Sand(SIZE)
+  sand.set_bg(BACK)
   sand.set_rgba(FRONT)
   fn = Fn(prefix='./res/', postfix='.png')
 
   make_creatures(sand)
-  # sand.set_bg_from_bw_array(bw)
+  # sand.set_bg(bw)
   name = fn.name()
   sand.write_to_png(name, GAMMA)
 
